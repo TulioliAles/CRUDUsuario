9c093eef4a541c238f148bb4d88bb81a5532592d
Alessandro Tulioli <alessandro.tulioli@csu.com.br>
2025-02-25 20:53:24 -0300

    Criacao Models, Interface, Servico e Controller

---
+++ CrudDapperUsuario/Services/IUsuarioInterface.cs
@@ -0,0 +1,10 @@
+﻿using CrudDapperUsuario.DTO;
+using CrudDapperUsuario.Models;
+
+namespace CrudDapperUsuario.Services
+{
+    public interface IUsuarioInterface
+    {
+        Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios();
+    }
+}
---
+++ CrudDapperUsuario/Services/UsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using CrudDapperUsuario.DTO;
+using CrudDapperUsuario.Models;
+
+namespace CrudDapperUsuario.Services
+{
+    public class UsuarioService : IUsuarioInterface
+    {
+        private readonly IConfiguration _configuration;
+
+        public UsuarioService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
